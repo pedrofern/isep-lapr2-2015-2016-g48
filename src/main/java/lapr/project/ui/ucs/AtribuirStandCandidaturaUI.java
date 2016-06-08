3d3e6fc2addf0e4f42010c0c9606ba489bd774f0
Tomas <Tomas@tomaspc>
2016-06-08 12:49:25 +0100

    Alterações no AtribuirStandCanddatura

--- src/main/java/lapr/project/ui/ucs/AtribuirStandCandidaturaUI.java
+++ src/main/java/lapr/project/ui/ucs/AtribuirStandCandidaturaUI.java
@@ -11,9 +11,12 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 
 /**
@@ -27,7 +30,7 @@ public class AtribuirStandCandidaturaUI extends JFrame {
     private static final int JANELA_ALTURA = 320;
     private JFrame framePai;
     private static final Dimension LABEL_TAMANHO = new JLabel("Area de Stand").getPreferredSize();
-
+    private JComboBox comboboxExposicao;
     
     public AtribuirStandCandidaturaUI() {
 
@@ -39,9 +42,66 @@ public class AtribuirStandCandidaturaUI extends JFrame {
     }
     
     public void criarComponentes() {
-        
+        add(criarPainelCentro(), BorderLayout.CENTER);
+        add(criarPainelOeste(), BorderLayout.WEST);
         add(criarPainelSul(), BorderLayout.SOUTH);
+        add(criarPainelEste(), BorderLayout.EAST);
     }
+    
+    private JPanel criarPainelCentro(){
+        JPanel p = new JPanel(new FlowLayout());
+        final int MARGEM_SUPERIOR = 0, MARGEM_INFERIOR = 0;
+        final int MARGEM_ESQUERDA = 10, MARGEM_DIREITA = 10;
+        p.setBorder(new EmptyBorder(MARGEM_SUPERIOR, MARGEM_ESQUERDA, MARGEM_INFERIOR, MARGEM_DIREITA));
+        p.setBorder(new TitledBorder("Stands"));
+
+        return p;
+    }
+    
+    private JPanel criarPainelEste(){
+        JPanel p = new JPanel(new FlowLayout());
+        final int MARGEM_SUPERIOR = 0, MARGEM_INFERIOR = 0;
+        final int MARGEM_ESQUERDA = 10, MARGEM_DIREITA = 10;
+        p.setBorder(new EmptyBorder(MARGEM_SUPERIOR, MARGEM_ESQUERDA, MARGEM_INFERIOR, MARGEM_DIREITA));
+        p.setBorder(new TitledBorder("Candidaturas"));
+
+        return p;
+    }
+    
+    private JPanel criarPainelOeste(){
+    JPanel p = new JPanel(new FlowLayout());
+        p.add(criarPainelExposicao());
+        final int MARGEM_SUPERIOR = 0, MARGEM_INFERIOR = 0;
+        final int MARGEM_ESQUERDA = 10, MARGEM_DIREITA = 10;
+        p.setBorder(new EmptyBorder(MARGEM_SUPERIOR, MARGEM_ESQUERDA, MARGEM_INFERIOR, MARGEM_DIREITA));
+        p.setBorder(new TitledBorder("Exposição"));
+
+        return p;
+    }
+    
+    private JPanel criarPainelExposicao() {
+
+        JPanel p = new JPanel(new FlowLayout());
+        
+        final int MARGEM_SUPERIOR = 0, MARGEM_INFERIOR = 0;
+        final int MARGEM_ESQUERDA = 0, MARGEM_DIREITA = 0;
+        p.setBorder(new EmptyBorder(MARGEM_SUPERIOR, MARGEM_ESQUERDA, MARGEM_INFERIOR, MARGEM_DIREITA));
+
+        p.add(getListaExposicao());
+        return p;
+
+    }
+    
+    private JComboBox getListaExposicao() {
+
+        comboboxExposicao = new JComboBox();
+        comboboxExposicao.setEditable(false);
+        comboboxExposicao.setSelectedIndex(-1);
+        comboboxExposicao.setPreferredSize(new Dimension(200, 20));
+
+        return comboboxExposicao;
+    }
+    
     private JPanel criarPainelSul() {
         JPanel p = new JPanel(new FlowLayout());
         p.setBorder(new TitledBorder("Opções"));
