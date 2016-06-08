0febdb6c7bc4ced3b9679682d2fe2fef26540a72
Edu <Edu@Edungelo>
2016-06-08 11:45:23 +0100

    Criação da modelo lista utilizadore e da lista utilizadores

---
+++ src/main/java/lapr/project/ui/ucs/ModeloListaUtilizadores.java
@@ -0,0 +1,48 @@
+package lapr.project.ui.ucs;
+
+
+import javax.swing.AbstractListModel;
+import lapr.project.model.*;
+
+public class ModeloListaUtilizadores extends AbstractListModel {
+
+    private ListaUtilizadores listaUtilizadores;
+    
+    public ModeloListaUtilizadores(ListaUtilizadores listaUtilizadores) {
+        this.listaUtilizadores = listaUtilizadores;
+    }
+
+    @Override
+    public int getSize() {
+        return listaUtilizadores.tamanho();
+    }
+
+    @Override
+    public Object getElementAt(int indice) {
+        return listaUtilizadores.obterUtilizador(indice);
+    }
+    
+    public boolean addElement(Utilizador utilizador){
+        boolean utilizadorAdicionado = listaUtilizadores.adicionarUtilizador(utilizador);
+        if(utilizadorAdicionado)
+            fireIntervalAdded(this, getSize()-1, getSize()-1);
+        return utilizadorAdicionado;
+    } 
+
+    public boolean removeElement(Utilizador utilizador){
+        int indice = listaUtilizadores.indiceDe(utilizador);
+        boolean utilizadorRemovido = listaUtilizadores.removerUtilizador(utilizador);
+        if(utilizadorRemovido)
+            fireIntervalRemoved(this, indice, indice);
+        return utilizadorRemovido;
+    }
+    
+    public boolean contains(Utilizador utilizador){
+        return listaUtilizadores.contem(utilizador);
+    }
+    
+    public void sort(){
+        listaUtilizadores.ordenarPorPosicao();
+    }
+
+}
