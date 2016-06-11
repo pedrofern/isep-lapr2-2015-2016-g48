41e47778264049b831ca7f1d2a568d5759adb06b
Pedro Fernandes <1060503@isep.ipp.pt>
2016-06-11 23:30:57 +0100

    [UC5] Teste unitário

---
+++ src/test/java/lapr/project/ui/ucs/RegistarCandidaturaUITest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.ui.ucs;
+
+import javax.swing.JButton;
+import javax.swing.JList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pedro Fernandes
+ */
+public class RegistarCandidaturaUITest {
+    
+    public RegistarCandidaturaUITest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getLstProdutos method, of class RegistarCandidaturaUI.
+     */
+    @Test
+    public void testGetLstProdutos() {
+        System.out.println("getLstProdutos");
+        RegistarCandidaturaUI instance = null;
+        JList expResult = null;
+        JList result = instance.getLstProdutos();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getBotaoRemoverProduto method, of class RegistarCandidaturaUI.
+     */
+    @Test
+    public void testGetBotaoRemoverProduto() {
+        System.out.println("getBotaoRemoverProduto");
+        RegistarCandidaturaUI instance = null;
+        JButton expResult = null;
+        JButton result = instance.getBotaoRemoverProduto();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
