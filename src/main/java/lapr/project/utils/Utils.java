20b109c4c47a9a0b257fc90c50609f662899c4d5
Eduangelo Ferreira <1151094@isep.ipp.pt>
2016-06-17 15:21:28 +0100

    [Cifra de César] (Implementação) Retrivied from https://pt.khanacademy.org/computing/computer-science/cryptography/crypt/v/one-time-pad - 17/06/2016

--- src/main/java/lapr/project/utils/Utils.java
+++ src/main/java/lapr/project/utils/Utils.java
@@ -64,7 +64,6 @@ public class Utils {
 //            }
 //        } while (true);
 //    }
-    
     /**
      * Obtem uma instancia de Date com a data atual.
      *
@@ -77,7 +76,6 @@ public class Utils {
         int dia = hoje.get(Calendar.DAY_OF_MONTH);
         return new Data(dia, mes, ano);
     }
-
 
     public static boolean confirma(String sMessage) {
         String strConfirma;
@@ -124,102 +122,162 @@ public class Utils {
     public static boolean isEmailValido(String email) {
         return email != null && EMAIL_PATTERN.matcher(email).matches();
     }
-    
-    
-    public static JComboBox criarComboExpo(RegistoExposicoes lista_exposicao){
-        Exposicao[] opcoes=lista_exposicao.getArray();
-        JComboBox comboExpos=new JComboBox();
-         comboExpos.setSelectedIndex(-1);
+
+    public static JComboBox criarComboExpo(RegistoExposicoes lista_exposicao) {
+        Exposicao[] opcoes = lista_exposicao.getArray();
+        JComboBox comboExpos = new JComboBox();
+        comboExpos.setSelectedIndex(-1);
         comboExpos.setEditable(false);
         comboExpos.setPreferredSize(new Dimension(200, 20));
-        DefaultComboBoxModel combo=new DefaultComboBoxModel(opcoes);
-        
+        DefaultComboBoxModel combo = new DefaultComboBoxModel(opcoes);
+
         comboExpos.setModel(combo);
-        
+
         return comboExpos;
     }
-    
-    public static JComboBox criarComboCand(ListaCandidaturas lista_candidaturas){
-        Candidatura[] opcoes=lista_candidaturas.getArray();
-        JComboBox comboCands=new JComboBox();
-        
-        DefaultComboBoxModel combo=new DefaultComboBoxModel(opcoes);
-        
+
+    public static JComboBox criarComboCand(ListaCandidaturas lista_candidaturas) {
+        Candidatura[] opcoes = lista_candidaturas.getArray();
+        JComboBox comboCands = new JComboBox();
+
+        DefaultComboBoxModel combo = new DefaultComboBoxModel(opcoes);
+
         comboCands.setModel(combo);
-        
+
         return comboCands;
     }
-    
-     public static JComboBox criarComboUser(RegistoUtilizadores lista_users){
-        Utilizador[] opcoes=lista_users.getArray();
-        JComboBox comboUsers=new JComboBox();
-        
-        DefaultComboBoxModel combo=new DefaultComboBoxModel(opcoes);
-        
+
+    public static JComboBox criarComboUser(RegistoUtilizadores lista_users) {
+        Utilizador[] opcoes = lista_users.getArray();
+        JComboBox comboUsers = new JComboBox();
+
+        DefaultComboBoxModel combo = new DefaultComboBoxModel(opcoes);
+
         comboUsers.setModel(combo);
-        
+
         return comboUsers;
     }
-    
-     public static JComboBox criarComboDemo(ListaDemonstracoes lista_demo){
-        Demonstracao[] opcoes=lista_demo.getArray();
-        JComboBox comboDemos=new JComboBox();
-        
-        DefaultComboBoxModel combo=new DefaultComboBoxModel(opcoes);
-        
+
+    public static JComboBox criarComboDemo(ListaDemonstracoes lista_demo) {
+        Demonstracao[] opcoes = lista_demo.getArray();
+        JComboBox comboDemos = new JComboBox();
+
+        DefaultComboBoxModel combo = new DefaultComboBoxModel(opcoes);
+
         comboDemos.setModel(combo);
-        
+
         return comboDemos;
     }
-     
-     
-      public static JComboBox criarComboStand(RegistoStands lista_stands){
-        Stand[] opcoes=lista_stands.getArray();
-        JComboBox comboStands=new JComboBox();
-        
-        DefaultComboBoxModel combo=new DefaultComboBoxModel(opcoes);
-        
+
+    public static JComboBox criarComboStand(RegistoStands lista_stands) {
+        Stand[] opcoes = lista_stands.getArray();
+        JComboBox comboStands = new JComboBox();
+
+        DefaultComboBoxModel combo = new DefaultComboBoxModel(opcoes);
+
         comboStands.setModel(combo);
-        
+
         return comboStands;
     }
-      
-        public static JComboBox criarComboRecurso(RegistoRecursos lista_recursos){
-        Recurso[] opcoes=lista_recursos.getArray();
-        JComboBox comboRecursos=new JComboBox();
-        
-        DefaultComboBoxModel combo=new DefaultComboBoxModel(opcoes);
-        
+
+    public static JComboBox criarComboRecurso(RegistoRecursos lista_recursos) {
+        Recurso[] opcoes = lista_recursos.getArray();
+        JComboBox comboRecursos = new JComboBox();
+
+        DefaultComboBoxModel combo = new DefaultComboBoxModel(opcoes);
+
         comboRecursos.setModel(combo);
-    
+
         return comboRecursos;
     }
-  
-        public static Data converterStringParaData(String data){
-            
-            String[] convertida =data.split("/");
-        
-            int diainicial = Integer.parseInt(convertida[0]);
-            int mesinicial = Integer.parseInt(convertida[1]);
-            int anoinicial = Integer.parseInt(convertida[2]);
-            
-            Data data_convertida=new Data(anoinicial, mesinicial, diainicial);
-            
-            return data_convertida;
-        
+
+    public static Data converterStringParaData(String data) {
+
+        String[] convertida = data.split("/");
+
+        int diainicial = Integer.parseInt(convertida[0]);
+        int mesinicial = Integer.parseInt(convertida[1]);
+        int anoinicial = Integer.parseInt(convertida[2]);
+
+        Data data_convertida = new Data(anoinicial, mesinicial, diainicial);
+
+        return data_convertida;
+
+    }
+
+    public static boolean validaDatasString(String data) {
+
+        String[] convertida = data.split("/");
+        int diainicial = Integer.parseInt(convertida[0]);
+        int mesinicial = Integer.parseInt(convertida[1]);
+        int anoinicial = Integer.parseInt(convertida[2]);
+
+        if (mesinicial >= 1 && mesinicial <= 12 && diainicial >= 1 && diainicial <= 31) {
+            return true;
         }
-        
-        public static boolean validaDatasString(String data){
-            
-            String[] convertida =data.split("/");
-            int diainicial = Integer.parseInt(convertida[0]);
-            int mesinicial = Integer.parseInt(convertida[1]);
-            int anoinicial = Integer.parseInt(convertida[2]);
-            
-            if(mesinicial>=1 && mesinicial<=12 && diainicial>=1 && diainicial<=31){
-                return true;
+
+        return false;
+    }
+
+    /**
+     * Retrivied from
+     * https://www.vivaolinux.com.br/script/JCriptoCesar-a-cifra-de-Cesar-em-Java/
+     * dia 17/06/2016//////////////////////////////////////////////////////////////
+     * Metodo que criptografa um texto, utilizando a famosa cifra
+     * de Cesar.
+     */
+    public static String encriptar(String texto) {
+        // Variavel que ira guardar o texto crifrado
+        StringBuilder textoCifrado = new StringBuilder();
+        // Variavel com tamanho do texto a ser encriptado
+        int tamanhoTexto = texto.length();
+
+        // Criptografa cada caracter por vez 
+        for (int c = 0; c < tamanhoTexto; c++) {
+            // Transforma o caracter em codigo ASCII e faz a criptografia
+            int letraCifradaASCII = ((int) texto.charAt(c)) + 26;
+
+            // Verifica se o codigo ASCII esta no limite dos caracteres imprimiveis
+            while (letraCifradaASCII > 126) {
+                letraCifradaASCII -= 94;
             }
-        
-            return false;
+
+            // Transforma codigo ASCII criptografado em caracter ao novo texto
+            textoCifrado.append((char) letraCifradaASCII);
         }
+
+        // Por fim retorna a mensagem criptografada por completo
+        return textoCifrado.toString();
+    }
+
+    /**
+     * Retrivied from
+     * https://www.vivaolinux.com.br/script/JCriptoCesar-a-cifra-de-Cesar-em-Java/
+     * dia 17/06/2016///////////////////////////////////////////////////////////////
+     * Metodo que descriptografa um texto, utilizando a famosa
+     * cifra Cesar.
+     */
+    public static String desencriptar(String textoCifrado) {
+        // Variavel que ira guardar o texto decifrado
+        StringBuilder texto = new StringBuilder();
+        // Variavel com tamanho do texto a ser decriptado
+        int tamanhoTexto = textoCifrado.length();
+
+        // Descriptografa cada caracter por vez
+        for (int c = 0; c < tamanhoTexto; c++) {
+            // Transforma o caracter em codigo ASCII e faz a descriptografia
+            int letraDecifradaASCII = ((int) textoCifrado.charAt(c)) - 26;
+
+            // Verifica se o codigo ASCII esta no limite dos caracteres imprimiveis
+            while (letraDecifradaASCII < 32) {
+                letraDecifradaASCII += 94;
+            }
+
+            // Transforma codigo ASCII descriptografado em caracter ao novo texto
+            texto.append((char) letraDecifradaASCII);
+        }
+
+        // Por fim retorna a mensagem descriptografada por completo
+        return texto.toString();
+    }
 }
