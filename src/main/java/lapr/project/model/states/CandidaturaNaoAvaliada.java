0ce93084a428fdb0f74d2d62af498920ecae73ca
Pedro Fernandes <1060503@isep.ipp.pt>
2016-06-19 21:06:14 +0100

    [Estados Candidatura (exp)] adicionei estado NaoAvaliada

---
+++ src/main/java/lapr/project/model/states/CandidaturaNaoAvaliada.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model.states;
+
+import lapr.project.model.Candidatura;
+
+/**
+ *
+ * @author Pedro Fernandes
+ */
+public class CandidaturaNaoAvaliada implements CandidaturaEstado{
+    
+    private final Candidatura candidatura;
+    
+    public CandidaturaNaoAvaliada(Candidatura candidatura){
+        this.candidatura=candidatura;                
+    }
+
+    @Override
+    public boolean validaEstadoCandidatura() {
+        return candidatura.getEstadoAtualCandidatura() instanceof CandidaturaEmAvaliacao;
+    }
+    
+    @Override
+    public String toString(){
+        return "CandidaturaNaoAvaliada";
+    }
+    
+}
