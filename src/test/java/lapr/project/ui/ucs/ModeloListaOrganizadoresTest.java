12b42c296d65f9268bc380350688d1d420da8a82
Eduangelo Ferreira <1151094@isep.ipp.pt>
2016-06-25 01:48:23 +0100

    [TesteUnitario] ModeloListaOrganizadores "realizei teste unitarios de alguns metodos que faltava desta classe".

--- src/test/java/lapr/project/ui/ucs/ModeloListaOrganizadoresTest.java
+++ src/test/java/lapr/project/ui/ucs/ModeloListaOrganizadoresTest.java
@@ -9,6 +9,7 @@ import lapr.project.model.CentroExposicoes;
 import lapr.project.model.Exposicao;
 import lapr.project.model.Organizador;
 import lapr.project.model.Utilizador;
+import lapr.project.model.lists.ListaOrganizadores;
 import lapr.project.utils.Data;
 import lapr.project.utils.Utils;
 import org.junit.After;
@@ -23,19 +24,18 @@ import static org.junit.Assert.*;
  * @author Diana
  */
 public class ModeloListaOrganizadoresTest {
-    
+
     private static final String s1 = "19/11/1111", s2 = "20/11/1111", s3 = "11/11/1111", s4 = "12/11/1111", s5 = "15/11/1111",
             s6 = "16/11/1111", s7 = "13/11/1111", s8 = "17/11/1111", s9 = "18/11/1111", s10 = "14/11/1111";
     private static Data d1, d2, d3, d4, d5, d6, d7, d8, d9, d10;
     private Exposicao e;
-    
+
     public ModeloListaOrganizadoresTest() throws Exception {
-        
-        CentroExposicoes ce=new CentroExposicoes();
- 
+
+        CentroExposicoes ce = new CentroExposicoes();
+
         e = ce.getRegistoExposicoes().novaExposicao();
 
-        
         d1 = Utils.converterStringParaData(s1);
         d2 = Utils.converterStringParaData(s2);
         d3 = Utils.converterStringParaData(s3);
@@ -47,23 +47,22 @@ public class ModeloListaOrganizadoresTest {
         d9 = Utils.converterStringParaData(s9);
         d10 = Utils.converterStringParaData(s10);
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-
 
     /**
      * Test of addElement method, of class ModeloListaOrganizadores.
@@ -71,41 +70,113 @@ public class ModeloListaOrganizadoresTest {
     @Test
     public void testAddElement() throws Exception {
         System.out.println("addElement ModeloListaOrganizadores");
-      
+
         String expResult = "Diana";
         Utilizador u = new Utilizador();
         u.setNome("Diana");
-        Organizador o=new Organizador(u);
-        
+        Organizador o = new Organizador(u);
+
         ModeloListaOrganizadores instance = new ModeloListaOrganizadores(e.getListaOrganizadores());
         instance.addElement(o);
 
-      
-        Organizador teste=e.getListaOrganizadores().getListaOrganizadores().get(0);
+        Organizador teste = e.getListaOrganizadores().getListaOrganizadores().get(0);
 
         String result = teste.getUtilizador().getNome();
         assertEquals(expResult, result);
-        
+
     }
- /**
+
+    /**
      * Test of getElement method, of class ModeloListaOrganizadores.
      */
     @Test
     public void testGetElement() throws Exception {
         System.out.println("getElement ModeloListaOrganizadores");
-      
+
         String expResult = "Diana";
         Utilizador u = new Utilizador();
         u.setNome("Diana");
-        Organizador o=new Organizador(u);
-        
+        Organizador o = new Organizador(u);
+
         ModeloListaOrganizadores instance = new ModeloListaOrganizadores(e.getListaOrganizadores());
         instance.addElement(o);
         instance.getElementAt(0);
-      
-        Organizador teste=e.getListaOrganizadores().getListaOrganizadores().get(0);
+
+        Organizador teste = e.getListaOrganizadores().getListaOrganizadores().get(0);
 
         String result = teste.getUtilizador().getNome();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of getSize method, of class ModeloListaOrganizadores.
+     */
+    @Test
+    public void testGetSize() {
+        System.out.println("getSize");
+        ModeloListaOrganizadores instance = new ModeloListaOrganizadores(e.getListaOrganizadores());
+        instance.addElement(new Organizador(new Utilizador("teste", "tese", "teste", "teste")));
+        int expResult = 1;
+        int result = instance.getSize();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of getRowCount method, of class ModeloListaOrganizadores.
+     */
+    @Test
+    public void testGetRowCount() {
+        System.out.println("getRowCount");
+        ModeloListaOrganizadores instance = new ModeloListaOrganizadores(e.getListaOrganizadores());
+        instance.addElement(new Organizador(new Utilizador("teste", "tese", "teste", "teste")));
+        int expResult = 1;
+        int result = instance.getRowCount();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of removeElement method, of class ModeloListaOrganizadores.
+     */
+    @Test
+    public void testRemoveElement() {
+        System.out.println("removeElement");
+        ModeloListaOrganizadores instance = new ModeloListaOrganizadores(e.getListaOrganizadores());
+        Organizador o =new Organizador(new Utilizador("teste", "tese", "teste", "teste"));
+        instance.addElement(o);
+        boolean expResult = false;
+        boolean result = instance.removeElement(o);
+
+    }
+
+    /**
+     * Test of contains method, of class ModeloListaOrganizadores.
+     */
+    @Test
+    public void testContains() {
+        System.out.println("contains");
+         ModeloListaOrganizadores instance = new ModeloListaOrganizadores(e.getListaOrganizadores());
+        Organizador o =new Organizador(new Utilizador("teste", "tese", "teste", "teste"));
+        instance.addElement(o);
+        boolean expResult = true;
+        boolean result = instance.contains(o);
+        assertEquals(expResult, result);
+       
+    }
+
+    /**
+     * Test of getListaOrganizadores method, of class ModeloListaOrganizadores.
+     */
+    @Test
+    public void testGetListaOrganizadores() {
+        System.out.println("getListaOrganizadores");
+         ModeloListaOrganizadores instance = new ModeloListaOrganizadores(e.getListaOrganizadores());
+        Organizador o =new Organizador(new Utilizador("teste", "tese", "teste", "teste"));
+        instance.addElement(o);
+        ListaOrganizadores expResult = instance.getListaOrganizadores();
+        ListaOrganizadores result = instance.getListaOrganizadores();
         assertEquals(expResult, result);
         
     }
